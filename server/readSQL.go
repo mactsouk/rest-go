bb1e0bab29082c0b186775cd60bb9ef7fbaca1e1
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-05-02 17:40:17 +0300

    Minor code changes

--- server/readSQL.go
+++ server/readSQL.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Reading from SQLite3")
+
+	arguments := os.Args
+	if len(arguments) != 2 {
+		fmt.Println("Need SQLite3 Database File")
+		return
+	}
+	database := arguments[1]
+
+	db, err := sql.Open("sqlite3", database)
+	if err != nil {
+		fmt.Println(nil)
+		return
+	}
 }
--- server/writeSQL.go
+++ server/writeSQL.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Writing to SQLite3")
+
+	arguments := os.Args
+	if len(arguments) != 2 {
+		fmt.Println("Need SQLite3 Database File")
+		return
+	}
+	database := arguments[1]
+
+	db, err := sql.Open("sqlite3", database)
+	if err != nil {
+		fmt.Println(nil)
+		return
+	}
+
+	fmt.Println("Emptying database table.")
+	_, err = db.Exec("DELETE FROM data")
+	if err != nil {
+		fmt.Println(nil)
+		return
+	}
+
 }
