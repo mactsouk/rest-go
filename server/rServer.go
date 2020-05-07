a19059ec209874ac3a6309f3c8ebb736f9f6a7ce
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-05-07 13:51:47 +0300

    Setting up Gorilla

--- server/rServer.go
+++ server/rServer.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/mactsouk/handlers"
 )
 
+// PORT is where the web server listens to
 var PORT = ":1234"
 
 func main() {
+	arguments := os.Args
+	if len(arguments) != 1 {
+		PORT = ":" + arguments[1]
+	}
+	fmt.Println("Listening to", PORT)
 
 	// Create a new ServeMux using Gorilla
 	mux := mux.NewRouter()
@@ -25,8 +32,17 @@ func main() {
 		IdleTimeout:  10 * time.Second,
 	}
 
-	mux.HandleFunc("/", handlers.DefaultHandler())
-	mux.HandleFunc("/time", handlers.TimeHandler())
+	mux.HandleFunc("/", handlers.DefaultHandler)
+
+	// Register GET
+	getMux := mux.Methods(http.MethodGet).Subrouter()
+	getMux.HandleFunc("/time", handlers.TimeHandler)
+
+	// Register PUT
+
+	// Register POST
+
+	// Register DELETE
 
 	err := s.ListenAndServe()
 	if err != nil {
