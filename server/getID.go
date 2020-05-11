e91023430a3e8948f4aac37914d57f15fed142ca
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-05-11 16:29:57 +0300

    RE on ID working

--- server/getID.go
+++ server/getID.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
 	"time"
 )
 
@@ -18,9 +20,26 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func idHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host)
-	Body := "Thanks for visiting!\n"
-	fmt.Fprintf(w, "%s", Body)
+	log.Println("ID Serving:", r.URL.Path, "from", r.Host)
+	reg := regexp.MustCompile(`/([0-9]+)`)
+	g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+	if len(g) != 1 {
+		log.Println("Invalid URI - More than one ID!")
+		http.Error(w, "Invalid URI", http.StatusBadRequest)
+		return
+	}
+
+	idText := g[0][1]
+	id, err := strconv.Atoi(idText)
+	if err != nil {
+		log.Println("Cannot convert to integer", idText)
+		http.Error(w, "Invalid URI", http.StatusBadRequest)
+		return
+	}
+
+	log.Println("ID:", id)
+	Body := "Looking for information about ID: " + idText
+	fmt.Fprintf(w, "%s\n", Body)
 }
 
 func main() {
@@ -40,6 +59,7 @@ func main() {
 
 	mux.Handle("/", http.HandlerFunc(defaultHandler))
 	mux.Handle("/id", http.HandlerFunc(idHandler))
+	mux.Handle("/id/", http.HandlerFunc(idHandler))
 
 	fmt.Println("Ready to serve at", PORT)
 	err := s.ListenAndServe()
