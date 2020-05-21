126b69f03bba9df89b3e667a8bf67671e63aa891
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-05-21 14:34:18 +0300

    Minor changes

--- client/cmd/root.go
+++ client/cmd/root.go
@@ -1,10 +1,13 @@
+/*
+Copyright © 2020 Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
+
+*/
 package cmd
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
+	"os"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
@@ -16,7 +19,12 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "client",
 	Short: "A brief description of your application",
-	Long:  ``,
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
