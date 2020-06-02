1447251f3cdefd90217d5db2a43424c955f54ce1
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-06-02 11:08:45 +0300

    Send and receive binary files

--- advanced/binary.go
+++ advanced/binary.go
@@ -1,9 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+var PORT = ":1234"
+
+func uploadFile(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func sendFile(rw http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
-	fmt.Println("Hello there!")
+	// Create a new ServeMux using Gorilla
+	mux := mux.NewRouter()
+
+	s := http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		ErrorLog:     nil,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  10 * time.Second,
+	}
+
+	// Register PUT
+	// Update User
+	putMux := mux.Methods(http.MethodPut).Subrouter()
+	putMux.HandleFunc("/upload/{id:[0-9]+}", uploadFile)
+
+	getMux := mux.Methods(http.MethodGet).Subrouter()
+	getMux.HandleFunc("/upload/{id:[0-9]+}", sendFile)
+
+	log.Println("Listening to", PORT)
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Printf("Error starting server: %s\n", err)
+		return
+	}
 }
