92eecf1bd49822cb172654fda6471bdf1eadc773
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-06-02 15:28:50 +0300

    Minor changes

--- advanced/server.go
+++ advanced/server.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"time"
 
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/gorilla/mux"
 )
 
 var SQLFILE string = ""
+var PORT string = ":1234"
 
 func main() {
 	log.Println("Hello!")
+
+	mux := mux.NewRouter()
+	s := http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		ErrorLog:     nil,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  10 * time.Second,
+	}
+
+	log.Println("Listening to", PORT)
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Printf("Error starting server: %s\n", err)
+		return
+	}
 }
