a5e689e035900e73542cbb426378b6b5064b8fe8
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-06-03 16:17:24 +0300

    2 Middleware functions - one of them for a subrouter

--- advanced/middleware.go
+++ advanced/middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -11,21 +12,42 @@ import (
 var PORT = ":1234"
 
 func timeHandler(rw http.ResponseWriter, r *http.Request) {
-
 }
 
 func addHandler(rw http.ResponseWriter, r *http.Request) {
+}
 
+func middleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Serving %s from %s", r.RequestURI, r.Host)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func anotherMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if len(PORT) != 0 {
+			log.Printf("Using port: %s", PORT)
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
+	arguments := os.Args
+	if len(arguments) >= 2 {
+		PORT = ":" + arguments[1]
+	}
 	mux := mux.NewRouter()
 
 	putMux := mux.Methods(http.MethodPut).Subrouter()
 	putMux.HandleFunc("/time", timeHandler)
 
 	getMux := mux.Methods(http.MethodGet).Subrouter()
-	getMux.Handle("/add", addHandler)
+	getMux.HandleFunc("/add", addHandler)
+	getMux.Use(anotherMiddleWare)
+
+	mux.Use(middleWare)
 
 	s := http.Server{
 		Addr:         PORT,
@@ -37,7 +59,7 @@ func main() {
 	}
 
 	log.Println("Listening to", PORT)
-	err = s.ListenAndServe()
+	err := s.ListenAndServe()
 	if err != nil {
 		log.Printf("Error starting server: %s\n", err)
 		return
