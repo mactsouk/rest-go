1667200a7273ad2fa50406ce5cc62dd6451d4015
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-06-03 15:21:23 +0300

    Begin implementing middleware functions

--- advanced/middleware.go
+++ advanced/middleware.go
@@ -1,9 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+var PORT = ":1234"
+
+func timeHandler(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func addHandler(rw http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
-	fmt.Println("Hello there!")
+	mux := mux.NewRouter()
+
+	putMux := mux.Methods(http.MethodPut).Subrouter()
+	putMux.HandleFunc("/time", timeHandler)
+
+	getMux := mux.Methods(http.MethodGet).Subrouter()
+	getMux.Handle("/add", addHandler)
+
+	s := http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		ErrorLog:     nil,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  10 * time.Second,
+	}
+
+	log.Println("Listening to", PORT)
+	err = s.ListenAndServe()
+	if err != nil {
+		log.Printf("Error starting server: %s\n", err)
+		return
+	}
 }
