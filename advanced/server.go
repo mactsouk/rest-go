e935764302531e435de93bacee07945c57c5c01d
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-06-03 18:26:52 +0300

    Update variable names in main function

--- advanced/server.go
+++ advanced/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -12,8 +13,28 @@ import (
 var SQLFILE string = ""
 var PORT string = ":1234"
 
+type notAllowedHandler struct{}
+
+func (h notAllowedHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	handlers.MethodNotAllowedHandler(rw, r)
+}
+
 func main() {
-	log.Println("Hello!")
+	arguments := os.Args
+	if len(arguments) == 2 || len(arguments) == 3 {
+		log.Println("Not enough arguments: DBNAME ")
+		return
+	}
+
+	if len(arguments) == 5 {
+		PORT = ":" + arguments[1]
+	}
+
+	err := handlers.CreateImageDirectory(IMAGESPATH)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	mux := mux.NewRouter()
 	s := http.Server{
@@ -25,11 +46,19 @@ func main() {
 		IdleTimeout:  10 * time.Second,
 	}
 
+	mux.NotFoundHandler = http.HandlerFunc(handlers.DefaultHandler)
+	notAllowed := notAllowedHandler{}
+	mux.MethodNotAllowedHandler = notAllowed
+
 	putMux := mux.Methods(http.MethodPut).Subrouter()
-	putMux.HandleFunc("/v2/files/{id:[0-9]+}", handlers.UploadFile)
+	putMux.HandleFunc("/v2/files/{filename:[a-zA-Z0-9][a-zA-Z0-9\\.]*[a-zA-Z0-9]}", handlers.UploadFile)
 
 	getMux := mux.Methods(http.MethodGet).Subrouter()
-	getMux.HandleFunc("/v2/files/{id:[0-9]+}", handlers.SendFile)
+	getMux.Handle(
+		"/v2/files/{filename:[a-zA-Z0-9][a-zA-Z0-9\\.]*[a-zA-Z0-9]}",
+		http.StripPrefix("/v2/files/", http.FileServer(http.Dir(IMAGESPATH))))
+
+	mux.Use(handers.MiddleWare)
 
 	log.Println("Listening to", PORT)
 	err := s.ListenAndServe()
