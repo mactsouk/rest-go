1a21a56010b0f49855d064ca1a8dcc0a9cc1ba0f
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-06-05 18:04:29 +0300

    Adding tests

--- advanced/server/server_test.go
+++ advanced/server/server_test.go
@@ -40,3 +40,71 @@ func TestTimeHanlderV2(t *testing.T) {
 			status, http.StatusOK)
 	}
 }
+
+func TestNotFoundV1(t *testing.T) {
+	req, err := http.NewRequest("GET", "/v1/doesNotExist", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlers.DefaultHandler)
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestNotFoundV2(t *testing.T) {
+	req, err := http.NewRequest("GET", "/v2/doesNotExist", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlers.DefaultHandler)
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestMethodNotAllowedV1(t *testing.T) {
+	req, err := http.NewRequest("DELETE", "/v1/time", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlers.MethodNotAllowedHandler)
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestMethodNotAllowedV2(t *testing.T) {
+	req, err := http.NewRequest("DELETE", "/v2/time", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlers.MethodNotAllowedHandler)
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
